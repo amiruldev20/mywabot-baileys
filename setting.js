c3c9941256301b87581713be5ec28eb3d959b84f
Abuzzpoet <76829545+Abuzzpoet@users.noreply.github.com>
2024-11-26 22:04:42 +0700

    Add files via upload

--- setting.js
+++ setting.js
@@ -4,13 +4,13 @@ pengaturan lainnya ada di db.setting. anda bisa gunakan command owner. default b
 Other settings are in db.setting. you can use the owner command. default language is id. do the command - db.setting.lang = "en" or any other language code to change it
 */
 export default {
-    owner: ["6285157489446"],
+    owner: ["6282171978174"],
     typedb: "json", // use json / mongo
 
     /* database setting */
     db: {
         local: "mywadb.json",
-        mongo: "url"
+        mongo: "mongodb+srv://rajifarmansyah46:rajif1707@cluster0.sndup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     }
 
 }
